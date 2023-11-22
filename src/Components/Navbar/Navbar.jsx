695a27409031c3772e917fb545ffafa172e9f93a
Arafath <arafath2092@gmail.com>
2023-11-23 00:52:44 +0530

    Done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,17 +7,11 @@ import Context from "../../Context/Context";
 import logo from "./mail.png";
 import "./Navbar.css";
 import { BiLogOutCircle } from "react-icons/bi";
-import { IoHome, IoSend, IoSettingsSharp } from "react-icons/io5";
-import { GoGraph } from "react-icons/go";
-import { CiLogout } from "react-icons/ci";
-import { TbListDetails } from "react-icons/tb";
 
-// Functional component for the navigation bar
 function NavComp() {
   const navigate = useNavigate();
   const contextData = useContext(Context);
 
-  // Function to handle user logout
   function handleLogout() {
     localStorage.clear();
     contextData.setNavFlag(false);
@@ -29,38 +23,21 @@ function NavComp() {
       <Navbar bg="dark" variant="dark">
         <Container className="">
           <Navbar.Brand onClick={() => navigate("/")} className="overflow-auto">
-            <b>
-              {window.innerWidth < 770 ? (
-                <img src={logo} alt="" className="imageStyle " />
-              ) : (
-                "BULK EMAIL TOOL"
-              )}
-            </b>
+            <img src={logo} alt="" className="imageStyle " />
+            <b>Bulk Email Tool</b>
           </Navbar.Brand>
           {contextData.navFlag ? (
             <>
               {" "}
               <Nav className="me-auto">
-                <Nav.Link onClick={() => navigate("/")} className="hover">
-                  {window.innerWidth < 770 ? <IoHome /> : "HOME"}
+                <Nav.Link onClick={() => navigate("/")}>HOME</Nav.Link>
+                <Nav.Link onClick={() => navigate("/compose")}>
+                  COMPOSE
                 </Nav.Link>
-                <Nav.Link
-                  onClick={() => navigate("/compose")}
-                  className="hover"
-                >
-                  {window.innerWidth < 770 ? <IoSend /> : "COMPOSE"}{" "}
-                </Nav.Link>
-                <Nav.Link onClick={() => navigate("/graph")} className="hover">
-                  {window.innerWidth < 770 ? <GoGraph /> : "GRAPH"}
-                </Nav.Link>
-                <Nav.Link onClick={() => navigate("/log")} className="hover">
-                  {window.innerWidth < 770 ? <TbListDetails /> : "LOG"}
-                </Nav.Link>
-                <Nav.Link
-                  onClick={() => navigate("/settings")}
-                  className="hover"
-                >
-                  {window.innerWidth < 770 ? <IoSettingsSharp /> : "SETTINGS"}
+                <Nav.Link onClick={() => navigate("/graph")}>GRAPH</Nav.Link>
+                <Nav.Link onClick={() => navigate("/log")}>LOG</Nav.Link>
+                <Nav.Link onClick={() => navigate("/settings")}>
+                  SETTINGS
                 </Nav.Link>
               </Nav>
               <button
@@ -72,7 +49,7 @@ function NavComp() {
                 data-bs-custom-class="custom-tooltip"
                 data-bs-title="This top tooltip is themed via CSS variables."
               >
-                {window.innerWidth < 770 ? <CiLogout /> : "LOGOUT"}
+                {window.innerWidth < 770 ? <BiLogOutCircle /> : "LogOut"}
               </button>
             </>
           ) : (
